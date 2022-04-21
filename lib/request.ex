6e55656bfc5fcb4b45002e9e70da9944d5b14d2e
Your Name <you@example.com>
2022-04-21 14:38:00 +0300

    Availability check against list

--- lib/request.ex
+++ lib/request.ex
@@ -1,14 +1,22 @@
 defmodule ServerRequest do
-  # require HTTPoison
-
-  def check() do
+  def check(url, [host, port]) do
     HTTPoison.start()
-    options = [proxy: "10.0.0.1:1080"]
-    HTTPoison.get!("ifconfig.me", [], options)
-    # HTTPoison.get!("ifconfig.me")
+
+    headers = [
+      {"accept",
+       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"},
+      {"accept-language", "en-US,en;q=0.9"},
+      {"cache-control", "no-cache"},
+      {"pragma", "no-cache"},
+      {"sec-fetch-dest", "document"},
+      {"sec-fetch-mode", "navigate"},
+      {"sec-fetch-site", "none"},
+      {"sec-fetch-user", "?1"},
+      {"sec-gpc", "1"},
+      {"upgrade-insecure-requests", "1"}
+    ]
+
+    options = [proxy: {:socks5, host, port}]
+    HTTPoison.get!(url, headers, options).body
   end
 end
-
-IO.inspect(ServerRequest.check())
-
-# HTTPoison.get("ifconfig.me")
--- lib/server_list_reachability.ex
+++ lib/server_list_reachability.ex
@@ -1,5 +1,7 @@
 defmodule ServerListReachability do
   require Poison
+  require HTTPoison
+  require ServerRequest
 
   def get_json(filename) do
     with {:ok, file_content} <- File.read(filename) do
@@ -23,6 +25,16 @@ defmodule ServerListReachability do
     )
   end
 
+  def filter_server_by_country_code(list, code) do
+    Enum.filter(
+      list,
+      fn
+        %{"country_code" => x} when x == code -> true
+        _ -> false
+      end
+    )
+  end
+
   def resolve_hostname(hostname) do
     case :inet.getaddr(hostname, :inet) do
       {:ok, ip} ->
@@ -33,22 +45,35 @@ defmodule ServerListReachability do
     end
   end
 
-  def get_socks(server) do
-    name = Map.get(server, "socks_name")
-    fullname = "#{name}"
-    # eip = resolve_hostname(to_charlist(fullname))
-    # "socks5 #{eip} 1080"
-    "socks5h://#{fullname}:1080"
-  end
-
   def list_servers() do
     get_json('servers.json')
+  end
+
+  def get_socks_host(server) do
+    name = Map.get(server, "socks_name")
+    "#{name}"
+  end
+
+  def get_socks(server) do
+    fullname = get_socks_host(server)
+    # eip = resolve_hostname(to_charlist(fullname))
+    # "socks5 #{eip} 1080"
+    "socks5://#{fullname}:1080"
+  end
+
+  def get_poison_proxy(server) do
+    host =
+      get_socks_host(server)
+      |> to_charlist()
+
+    [host, 1080]
   end
 
   def result() do
     get_json('servers.json')
     |> filter_servers
-    |> Enum.map(&get_socks(&1))
+    |> filter_server_by_country_code("us")
+    |> Enum.map(&get_poison_proxy(&1))
   end
 
   def write_file() do
@@ -60,8 +85,24 @@ defmodule ServerListReachability do
 
     File.write!("socks5.txt", content)
   end
+
+  def try_server(hostname, proxy) do
+    result = ServerRequest.check(hostname, proxy)
+    IO.puts("Accessed with #{List.first(proxy)}: {#{result}}")
+  end
+
+  def check_server(hostname, country_code) do
+    proxy_servers =
+      get_json('servers.json')
+      |> filter_servers
+      |> filter_server_by_country_code(country_code)
+      |> Enum.map(&get_poison_proxy(&1))
+
+    proxy_servers
+    |> Enum.each(&try_server(hostname, &1))
+  end
 end
 
-# IO.puts(inspect(ServerListReachability.result()))
+# ServerListReachability.check_server("https://ifconfig.me", "us")
 # ServerListReachability.write_file()
-# IO.inspect(ServerListReachability.list_servers())
+# IO.inspect(ServerListReachability.result())
